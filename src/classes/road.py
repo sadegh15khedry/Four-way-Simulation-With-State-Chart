4dc191b83450238825777b57fc028bf724e64efd
sadegh15khedry <sadegh15khedry@gmail.com>
2024-11-25 16:34:16 +0330

    road generation added

--- src/classes/road.py
+++ src/classes/road.py
@@ -2,5 +2,9 @@ class Road:
     def __init__(self, id, direction):
         self.id = id
         self.four_ways = []
+        # 1 for up
+        # 2 for right
+        # 3 for down
+        # 4 for left
         self.direction = direction
     
--- src/scripts/simulation.py
+++ src/scripts/simulation.py
@@ -1,2 +1,34 @@
-def simulate(M, N, max_time_steps):
+from vehicle import Vehicle
+from road import Road
+
+def get_new_direction(direction):
+    if direction == 1:
+        return 3
+    elif direction == 3:
+        return 1
+    elif direction == 2:
+        return 4
+    elif direction == 4:
+        return 2
+
+def initialize(M, N):
+    i=0
+    direction = 1
+    while i < M:
+        Road(i, direction)
+        print(f"vehicle {i} generated direction {direction}")
+        direction = get_new_direction(direction)
+        i += 1
     
+    direction = 2    
+    while i < M + N:
+        Road(i, direction)
+        print(f"vehicle {i} generated direction {direction}")
+        direction = get_new_direction(direction)
+        i += 1
+
+        
+def simulate(m, n, max_time_steps):
+    initialize(m,n)
+    for i in range(max_time_steps):
+        pass
